3ce7c650f6d7b48196f2846519869b4a8f0c80f0
CGARCHER <cipri.profesor.informatica@gmail.com>
2025-09-23 19:59:51 +0200

    listado de mascotas
    listado de mascotas

--- src/main/java/com/hellin/despliegue_api_rest/controller/PetControlller.java
+++ src/main/java/com/hellin/despliegue_api_rest/controller/PetControlller.java
@@ -1,14 +1,23 @@
 package com.hellin.despliegue_api_rest.controller;
 
+import com.hellin.despliegue_api_rest.entity.Pet;
+import com.hellin.despliegue_api_rest.repository.PetRepository;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PetControlller {
 
+    private PetRepository petRepository;
+
+    public PetControlller(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
     @GetMapping
-    public String hello(){
-        return "Hello World";
+    public List<Pet> hello(){
+        return petRepository.findAll();
     }
 }
