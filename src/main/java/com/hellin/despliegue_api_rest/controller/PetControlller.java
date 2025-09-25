75d6bc7f555066de2b2c8bc685b26077a40c53c2
CGARCHER <cipri.profesor.informatica@gmail.com>
2025-09-25 19:20:33 +0200

    Front
    Front

--- src/main/java/com/hellin/despliegue_api_rest/controller/PetControlller.java
+++ src/main/java/com/hellin/despliegue_api_rest/controller/PetControlller.java
@@ -3,11 +3,13 @@ package com.hellin.despliegue_api_rest.controller;
 import com.hellin.despliegue_api_rest.entity.Pet;
 import com.hellin.despliegue_api_rest.repository.PetRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/pet")
 public class PetControlller {
 
     private PetRepository petRepository;
@@ -16,7 +18,7 @@ public class PetControlller {
         this.petRepository = petRepository;
     }
 
-    @GetMapping
+    @GetMapping("/list")
     public List<Pet> hello(){
         return petRepository.findAll();
     }
